8f72e5946d9f6528de82f2367bef7327ce21fc54
sibeil Rock <b00156196@mytudublin.ie>
2025-04-22 20:52:34 +0100

    these are the html pages and the updated views and urls lists

--- edstaff/urls.py
+++ edstaff/urls.py
@@ -5,6 +5,21 @@ from . import views
 
 urlpatterns = [
 
-    path('',views.home,name='' ),
+    path('',views.home,name='index' ),
+    path('home/',views.home,name='home' ),
+
+    path('register/',views.register,name='register' ),
+    path('profile/',views.profile,name='profile' ),
+    path('logout/',views.login, name='logout' ),
+
+    path('index/',views.login, name='login' ),
+    path ('job_list/',views.login, name='job_list' ),
+    path('about/',views.about,name='about' ),
+    path('contact/',views.contact,name='contact' ),
+    path('company/',views.company,name='company' ),
+    path('job_create/',views.job_create,name='job_create' ),
+    path('applicant/',views.applicant,name='applicant' ),
+    path('job/',views.job,name='job' ),
+
 
 ]
--- edstaff/views.py
+++ edstaff/views.py
@@ -4,7 +4,41 @@ def home(request):
 
     return render(request, 'edstaff/index.html')
 
+def register(request):
+    return render(request, 'edstaff/register.html')
+def profile(request):
+    return render(request, 'edstaff/profile.html')
+def logout(request):
+    return render(request, 'edstaff/logout.html')
+def job_list(request):
+    return render(request, 'edstaff/job_list.html')
+def job_create(request):
+    return render(request, 'edstaff/job_create.html')
+def job_update(request):
+    return render(request, 'edstaff/job_update.html')
+def job_delete(request):
+    return render(request, 'edstaff/job_delete.html')
+def job_view(request):
+    return render(request, 'edstaff/job_view.html')
+def job_apply(request):
+    return render(request, 'edstaff/job_apply.html')
+def job_accept(request):
+    return render(request, 'edstaff/job_accept.html')
+def job_reject(request):
+    return render(request, 'edstaff/job_reject.html')
+def about(request):
 
+    return render(request, 'edstaff/about.html')
+def contact(request):
+    return render(request, 'edstaff/contact.html')
+def login(request):
 
+    return render(request, 'edstaff/login.html')
+def company(request):
+    return render(request, 'edstaff/company.html')
+def applicant(request):
 
+    return render(request, 'edstaff/applicant.html')
+def job(request):
+    return render(request, 'edstaff/job.html')
 
